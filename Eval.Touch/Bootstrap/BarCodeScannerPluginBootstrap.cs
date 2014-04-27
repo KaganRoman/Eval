ed69b4e4605cb3cb401cd9e469febe4a06de11bb
Roman Kagan <roman@rumble.me>
2014-04-27 16:25:09 +0300

    Add plugins

---
+++ Eval.Touch/Bootstrap/BarCodeScannerPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Eval.Touch.Bootstrap
+{
+    public class BarCodeScannerPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Acr.MvvmCross.Plugins.BarCodeScanner.PluginLoader, Acr.MvvmCross.Plugins.BarCodeScanner.Touch.Plugin>
+    {
+    }
+}
---
+++ Eval.Touch/Bootstrap/EmailPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Eval.Touch.Bootstrap
+{
+    public class EmailPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Email.PluginLoader, Cirrious.MvvmCross.Plugins.Email.Touch.Plugin>
+    {
+    }
+}
